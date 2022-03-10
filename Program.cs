b7333fcc1fda3bfe182a1fea3bc41267dca301c1
Gerry William <gwilliam@student.telkomuniversity.ac.id>
2022-03-10 20:46:17 +0700

    menambahkan implementasi dengan table-driven

--- Program.cs
+++ Program.cs
@@ -1,16 +1,26 @@
 ﻿using System;
 
-namespace tpmodul3 // Note: actual namespace depends on the project name.
+namespace tpmodul3_1302204126
 {
     class Program
     {
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            KodePos KodePos = new KodePos();
+            KodePos.getKodePos("Batununggal", "40266");
+            KodePos.getKodePos("Kujangsari", "40287");
+            KodePos.getKodePos("Mengger", "40267");
+            KodePos.getKodePos("Wates", "40256");
+            KodePos.getKodePos("Cijaura", "40287");
+            KodePos.getKodePos("Jatisari", "40286");
+            KodePos.getKodePos("Margasari", "40286");
+            KodePos.getKodePos("Sekejati", "40286");
+            KodePos.getKodePos("Kebonwaru", "40272");
+            KodePos.getKodePos("Maleer", "40274");
+            KodePos.getKodePos("Samoja", "40273");
         }
     }
 
-    class KodePos
-    
+
 }
